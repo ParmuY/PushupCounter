4f90b9777c0beaea848001d9b8e871dbd60d379f
ParmuY <pramesh2151@gmail.com>
2021-01-30 08:23:07 +0530

    fixing dialogbox for highscore

--- app/src/main/java/com/parmu/pushupcounter/DialogHighScore.java
+++ app/src/main/java/com/parmu/pushupcounter/DialogHighScore.java
@@ -2,10 +2,13 @@ package com.parmu.pushupcounter;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,12 +18,12 @@ public class DialogHighScore extends AppCompatDialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
         ViewGroup viewGroup = getActivity().findViewById(android.R.id.content);
         View dialogView = LayoutInflater.from(getActivity()).inflate(R.layout.custom_dialog_box_view,viewGroup,false);
         dialogBuilder.setView(dialogView)
                 .setCancelable(true);
-
         return dialogBuilder.create();
     }
 }
