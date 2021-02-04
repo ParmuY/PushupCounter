3a19833091b4d21454daf39bcaa5aca0c2de4963
ParmuY <pramesh2151@gmail.com>
2021-02-04 17:03:54 +0530

    splash screen introduction

--- app/src/main/java/com/parmu/pushupcounter/SplashActivity.java
+++ app/src/main/java/com/parmu/pushupcounter/SplashActivity.java
@@ -2,7 +2,14 @@ package com.parmu.pushupcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +17,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Intent mIntent = new Intent(SplashActivity.this,MainActivity.class);
+//        Timer timerSplash = new Timer();
+//        timerSplash.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                startActivity(mIntent);
+//            }
+//        },5000);
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab_splash_screen);
+        floatingActionButton.setOnClickListener(view -> {
+            startActivity(mIntent);
+        });
+
     }
 }
