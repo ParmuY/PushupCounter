3cad621901a230327a12a099c30989a713b06668
ParmuY <pramesh2151@gmail.com>
2021-02-09 17:04:55 +0530

    started working on SitupActivity

---
+++ app/src/main/java/com/parmu/pushupcounter/SitupActivity.java
@@ -0,0 +1,14 @@
+package com.parmu.pushupcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SitupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_situp);
+    }
+}
